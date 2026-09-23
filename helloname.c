94e70ae26b8fda4e6040113fcf1a2abf48582cdf
jbvgjgfhfj <2425369983@qq.com>
2026-09-23 19:17:45 +0800

    hello name

---
+++ helloname.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    char firstname[20];
+    char lastname[20];
+    scanf("%s",firstname);
+    scanf("%s",lastname);
+    printf("hello %s %s",firstname,lastname);
+    return 0;    
+}
